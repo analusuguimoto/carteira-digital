c16c88c0a9ca4b7c33284e23d8dde12aa6926845
Ana Lourenco <analuizajl5@gmail.com>
2023-08-08 15:29:34 -0300

    req 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,45 @@
+import { useSelector } from 'react-redux';
+
 function Table() {
-  return <div>Table</div>;
+  const wallet = useSelector((state: any) => state.wallet);
+  const { expenses } = wallet;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((exp: any) => {
+          return (
+            <tr key={ exp.id }>
+              <td>{ exp.description }</td>
+              <td>{ exp.tag }</td>
+              <td>{ exp.method }</td>
+              <td>{ Number(exp.value).toFixed(2) }</td>
+              <td>{ exp.exchangeRates[exp.currency].name }</td>
+              <td>{ Number(exp.exchangeRates[exp.currency].ask).toFixed(2) }</td>
+              <td>
+                { (Number(exp.exchangeRates[exp.currency].ask) * Number(exp.value))
+                  .toFixed(2) }
+              </td>
+              <td>Real</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
